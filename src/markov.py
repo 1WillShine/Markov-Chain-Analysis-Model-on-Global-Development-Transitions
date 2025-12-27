7710fbcd5437db25ebc6a1598c7000e52371f4b9
Will <hosoos@gmail.com>
2025-12-26 18:25:01 -0800

    Refactor transition matrix build process

--- src/markov.py
+++ src/markov.py
@@ -1,24 +1,29 @@
 import pandas as pd
-import numpy as np
-from data_loader import df_final
-df = df_final
-STAGES = ["Least Developed", "Developing", "Emerging", "Advanced"]
+
+STAGES = [
+    "Least Developed",
+    "Developing",
+    "Emerging",
+    "Advanced"
+]
 
 def build_transition_matrix(df):
     df = df.sort_values(["Country Code", "Year"])
-    #df["stage"] = df["dev_score"].apply(dev_stage)
 
     transitions = []
 
-    for country, g in df.groupby("Country Code"):
-        prev = g["stage"].shift(1)
-        curr = g["stage"]
+    for _, g in df.groupby("Country Code"):
+        prev = g["dev_state"].shift(1)
+        curr = g["dev_state"]
+
         mask = prev.notna()
-        transitions += list(zip(prev[mask], curr[mask]))
+        transitions.extend(zip(prev[mask], curr[mask]))
 
     T = pd.DataFrame(0, index=STAGES, columns=STAGES)
 
     for i, j in transitions:
-        T.loc[i, j] += 1
+        if i in STAGES and j in STAGES:
+            T.loc[i, j] += 1
 
     return T.div(T.sum(axis=1), axis=0).fillna(0)
+
